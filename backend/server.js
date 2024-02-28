25a1581b2a2d1f91c1d15a85c2bf45cce7f86477
aurelioobando <aureliosflow@gmail.com>
2024-02-28 17:26:29 -0600

    delete: delete ambiguos variable

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const {app, server, server} = require("./app")
+const {app, server} = require("./app")
 const server = server.listen( app.get('port'), ()=>{
     console.log(`server listening on port ${app.get('port')}`)
 })
