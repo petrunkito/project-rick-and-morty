deea83c1a7fa73f22640db5e9bb9d1c26959a96d
aurelioobando <aureliosflow@gmail.com>
2024-02-29 21:53:50 -0600

    add: save change

--- frontend/public/shared/apiBackend.js
+++ frontend/public/shared/apiBackend.js
@@ -1,29 +1,56 @@
+/**
+ * este archivo sera el encargado de usar los metodos de la api del backend,
+ * aqui podremos consultar favoritos, obtener un favorito, crear un favorito
+ */
+
+//esta es la url de la api
 const urlApi = `http://localhost:3000/api/favorites/`
+
+
+/**
+ *este metodo obtiene todos los personajes que se agregaron a los favoritos.
+ * @returns {Array} retorna un erreglo con los personajes favoritos
+ */
 const getAll = async () => {
     try {
-
+        //hacemos la peticion
         let response = await fetch(urlApi)
+        //si la peticion no se concreto arrojamos el siguiente error
         if (!response.ok) throw new Error('data collection failed')
-        let data = (await response.json()).result
-        return data
+        //retornamos los personajes favoritos
+        return (await response.json()).result
     } catch (err) {
-        console.log(err)
+        return false
     }
 }
 
+/**
+ * este metodo obtiene a un solo personaje favorito de la base de datos
+ * @param {Number} id - el id del personaje 
+ * @returns {Object} retorna un objeto con la informacion del persona
+ */
 const getOne = async (id) => {
     try {
+        //hacemos la peticion
         let response = await fetch(urlApi + id)
+        //si la peticion no se concreto arrojamos el siguiente error
         if (!response.ok) throw new Error('data collection failed')
-        let data = await response.json()
-        return data
+        //retornamos el personaje seleccionado
+        return (await response.json()).result
     } catch (err) {
-        console.log(err)
+        return false
     }
 }
 
+//
+/**
+ * este metodo crea o agrega un nuevo personaje favorito al backend
+ * @param {Object} body - contiene todos los campos a insertar del personaje en favoritos
+ * @returns {Boolean} retorna true si todo salio bien, y de lo contrario retorna false
+ */
 const create = async (body) => {
     try {
+
         let response = await fetch(urlApi, {
             method: 'POST',
             headers: {
@@ -32,11 +59,9 @@ const create = async (body) => {
             body: JSON.stringify(body)
         })
         if (!response.ok) throw new Error('data collection failed')
-        let data = await response.json()
-        console.log("data: ", data)
-        return data
+        return true
     } catch (err) {
-        console.log(err)
+        return false
     }
 }
 
@@ -49,7 +74,7 @@ const deleteOne = async (id) => {
         let data = await response.json()
         console.log("data: ", data)
     }catch(err){
-        console.log(err)
+        return false
     }
 }
 
